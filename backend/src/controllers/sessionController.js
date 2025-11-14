78d8dd2e24e25e137f69537d7f474de1504dc216
Madhankumar00 <madhanprs@gmail.com>
2025-11-14 16:44:24 +0530

    refactoring sessionController.js file

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -111,13 +111,13 @@ export async function joinSession(req, res) {
 
     if (!session) return res.status(404).json({ message: "Session not found" });
 
-    // if (session.status !== "active") {
-    //   return res.status(400).json({ message: "Cannot join a completed session" });
-    // }
+    if (session.status !== "active") {
+      return res.status(400).json({ message: "Cannot join a completed session" });
+    }
 
-    // if (session.host.toString() === userId.toString()) {
-    //   return res.status(400).json({ message: "Host cannot join their own session as participant" });
-    // }
+    if (session.host.toString() === userId.toString()) {
+      return res.status(400).json({ message: "Host cannot join their own session as participant" });
+    }
 
     // check if session is already full - has a participant
     if (session.participant)
@@ -165,7 +165,7 @@ export async function endSession(req, res) {
     await session.save();
 
     res.status(200).json({ session, message: "Session ended successfully" });
-    
+
   } catch (error) {
     console.log("Error in endSession controller:", error.message);
     res.status(500).json({ message: "Internal Server Error" });
